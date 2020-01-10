e38a707bd6fe12e31f7039af03a48b4f8494a19f
lucasforteokl <lucas.forte@oklgroup.net>
2020-01-10 11:30:39 -0300

    second task done

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 const btnOne = document.getElementById('action01');
+const btnTwo = document.getElementById('action02');
 
 function removeA() {
   const words = document.getElementById('target01').innerHTML;
@@ -8,3 +9,17 @@ function removeA() {
 }
 
 btnOne.addEventListener('click', removeA)
+
+function changeCharacter(param1, param2) {
+  const words = document.getElementById('target02').innerHTML;
+  param1 = document.getElementById('c1').value;
+  param2 = document.getElementById('c2').value;
+  const change = param1; 
+
+  let regex = new RegExp(`${change}`, 'g')
+  let wordsWhitoutA = words.replace(regex, `${param2}`);
+
+  document.getElementById("target02").innerHTML = wordsWhitoutA;
+}
+
+btnTwo.addEventListener('click', changeCharacter)
